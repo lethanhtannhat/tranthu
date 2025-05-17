f017748ac2afdbacc0afe375cca75b29a96f3427
lethanhtannhat <146319062+lethanhtannhat@users.noreply.github.com>
2025-05-17 08:35:57 +0700

    Update khaosatcheogithub.py

--- khaosatcheogithub.py
+++ khaosatcheogithub.py
@@ -286,7 +286,7 @@ submit_button.click()
 time.sleep(1)
 submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div[2]')))
 submit_button.click()
-time.sleep(1)
+time.sleep(100)
 with open("index.txt", "w") as f:
     f.write(str(index + 1))
      
